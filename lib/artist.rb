e78302136e2ed3b3d6662ec018d94a5779c27f6b
Ashley Twomey <aprier091012@gmail.com>
2019-12-09 16:26:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,12 @@
 class Artist
-  attr_accessor :name
-  @@song_count = 1
-  
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
   def initialize(name)
     @name = name
-    @songs = [] 
-  end
-
-  def songs
-    @songs
+    @songs = []
   end
 
   def add_song(song)
@@ -17,8 +15,8 @@ class Artist
     @@song_count += 1
   end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
